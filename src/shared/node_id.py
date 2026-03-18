3de4dc6425a869e49c736c649d6c2d34b8276dce
domster704 <domster704@mail.ru>
2026-03-18 12:09:45 +0300

    fix get_node_id

--- src/shared/node_id.py
+++ src/shared/node_id.py
@@ -5,7 +5,7 @@ from src.config.settings import settings
 
 
 def get_node_id() -> str:
-    port = settings.server.port
+    port = settings.port
     hostname = socket.gethostname()
     node_id = os.getenv("NODE_ID", f"{hostname}_{port}")
     return node_id
